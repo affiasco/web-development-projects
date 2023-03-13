d874331bb8d587c187276eb0ad595c1923294e67
Andrew <affiasco@umich.edu>
2023-03-13 16:19:56 -0400

    Create playRound function with if/else statements to determine a winner of an RPS game, output player and computer choices to console as well as the winner.

--- odin_project/odin_foundations/rock_paper_scissors/rps.js
+++ odin_project/odin_foundations/rock_paper_scissors/rps.js
@@ -3,4 +3,28 @@ function getComputerChoice() {
   return rpsChoices[Math.floor(Math.random() * rpsChoices.length)];
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+  let ps = playerSelection.toLowerCase();
+  let cs = computerSelection.toLowerCase();
+
+  console.log(
+    `Player: ${ps.charAt(0).toUpperCase() + ps.slice(1)} \nComputer: ${
+      cs.charAt(0).toUpperCase() + cs.slice(1)
+    }`
+  );
+
+  if (ps === "paper" && cs === "scissors") {
+    return "You Lose! Scissors beat Paper";
+  } else if (ps === "scissors" && cs === "rock") {
+    return "You Lose! Rock beats Scissors";
+  } else if (ps === "rock" && cs === "paper") {
+    return "You Lose! Paper beats Rock";
+  } else if (ps === cs) {
+    return "Tie! Try again...";
+  } else {
+    return `You win! ${ps.charAt(0).toUpperCase() + ps.slice(1)} beats ${
+      cs.charAt(0).toUpperCase() + cs.slice(1)
+    }`;
+  }
+}
+console.log(playRound("rocK", getComputerChoice()));
