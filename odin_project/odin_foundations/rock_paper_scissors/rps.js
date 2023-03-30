ccfca7f3e986439861038a9d9b2c99d7c70e04fe
Andrew <affiasco@umich.edu>
2023-03-29 20:50:29 -0400

    Complete styling page, adjust images

--- odin_project/odin_foundations/rock_paper_scissors/rps.js
+++ odin_project/odin_foundations/rock_paper_scissors/rps.js
@@ -1,24 +1,28 @@
+const resultsContainer = document.querySelector(".results-container");
+const results = document.querySelector(".results");
 const btnContainer = document.querySelector(".button-container");
-const playAgain = document.querySelector(".play-again-container");
+const playAgain = document.querySelector(".play-again");
+const winnerDiv = document.querySelector(".winner");
+const bmGif = document.querySelector(".bm-gif");
 const choices = document.querySelectorAll(".choice");
+const scoreContainer = document.querySelector(".score-container");
+const score = document.querySelector(".score");
 let playerScore = 0;
 let compScore = 0;
 
+scoreContainer.appendChild(score);
+
 function getComputerChoice() {
-  const rpsChoices = ["Rock", "Paper", "Scissors"];
+  const rpsChoices = ["Rock", "Paper", "Metal"];
   return rpsChoices[Math.floor(Math.random() * rpsChoices.length)];
 }
 
 function displayMessage(message) {
-  const resultsContainer = document.querySelector(".results-container");
-  const results = document.querySelector(".results");
   results.textContent = message;
   resultsContainer.replaceChild(results, results);
 }
 
 function scoreMessaging(message) {
-  const scoreContainer = document.querySelector(".score-container");
-  const score = document.querySelector(".score");
   score.textContent = message;
   scoreContainer.replaceChild(score, score);
 }
@@ -30,31 +34,33 @@ function updateScore(roundWinner = null) {
     ++compScore;
   }
 
-  scoreMessaging(`Player Score: ${playerScore}, Computer Score: ${compScore}`);
+  scoreMessaging(`METALHEAD: ${playerScore} CORPORATE: ${compScore}`);
 }
 
 function playRound(e) {
+  winnerDiv.classList.remove("invisible");
+  results.classList.remove("invisible");
   let ps = e.target.dataset.rps;
 
   if (ps === null || ps === undefined) return;
 
   const cs = getComputerChoice().toLowerCase();
 
-  if (ps === "paper" && cs === "scissors") {
-    displayMessage("You Lose! Scissors beat Paper");
+  if (ps === "paper" && cs === "metal") {
+    displayMessage("You LOSE! Metal BEATS Paper");
     updateScore("computer");
-  } else if (ps === "scissors" && cs === "rock") {
-    displayMessage("You Lose! Rock beats Scissors");
+  } else if (ps === "metal" && cs === "rock") {
+    displayMessage("You LOSE! Rock BEATS Metal");
     updateScore("computer");
   } else if (ps === "rock" && cs === "paper") {
-    displayMessage("You Lose! Paper beats Rock");
+    displayMessage("You LOSE! Paper BEATS Rock");
     updateScore("computer");
   } else if (ps === cs) {
     displayMessage("Tie! Try again...");
     updateScore();
   } else {
     displayMessage(
-      `You win! ${ps.charAt(0).toUpperCase() + ps.slice(1)} beats ${
+      `You WIN! ${ps.charAt(0).toUpperCase() + ps.slice(1)} BEATS ${
         cs.charAt(0).toUpperCase() + cs.slice(1)
       }`
     );
@@ -75,6 +81,7 @@ function endGame() {
 
   if (playerScore === 5 && compScore < 5) {
     winner.textContent = "Amazing...you've done it! YOU WIN!";
+    bmGif.classList.remove("no-display");
     cleanUp();
   } else if (compScore === 5 && playerScore < 5) {
     winner.textContent = "Better Luck next time. Computer wins...";
